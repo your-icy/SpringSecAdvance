54060c42b0b9668fd975b0363f365062a9bb6717
your-icy <dev.harshpal@gmail.com>
2024-07-02 04:48:12 +0530

    "Fucked up config"

--- SpringSecurity/src/main/java/com/icycodes/springsecurity/SpringSecurityApplication.java
+++ SpringSecurity/src/main/java/com/icycodes/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package com.icycodes.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.icycodes")
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
